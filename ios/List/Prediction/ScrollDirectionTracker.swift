bec407cd88ec3b0d4410d3a0fee49f69cb5040aa
Shiv Shankar Tiwari <pythonsst@gmail.com>
2026-01-04 06:03:22 +0530

    fix: type

--- ios/List/Prediction/ScrollDirectionTracker.swift
+++ ios/List/Prediction/ScrollDirectionTracker.swift
@@ -1,27 +1,27 @@
 import CoreGraphics
 
-enum ScrollDirection {
+enum ScrollMotion {
   case forward
   case backward
   case none
 }
-
 final class ScrollDirectionTracker {
 
   private var lastOffset: CGFloat = 0
-  private(set) var direction: ScrollDirection = .none
+  private(set) var motion: ScrollMotion = .none
 
   func update(offset: CGFloat) {
     if offset > lastOffset {
-      direction = .forward
+      motion = .forward
     } else if offset < lastOffset {
-      direction = .backward
+      motion = .backward
     }
     lastOffset = offset
   }
 
   func reset() {
     lastOffset = 0
-    direction = .none
+    motion = .none
   }
 }
+
